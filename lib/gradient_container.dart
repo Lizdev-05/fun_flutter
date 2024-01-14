c602110751c9a0f66e654b3e74fb29439d7557c4
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-01-14 08:46:53 +0000

    Add opacity to my image through color

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -25,6 +25,7 @@ class GradientContainer extends StatelessWidget {
               Image.asset(
                 "assets/images/quiz-logo.png",
                 width: 300,
+                color: const Color.fromARGB(150, 255, 255, 255),
               ),
               const SizedBox(height: 80),
               const Text(
