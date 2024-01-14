019060972bc11a7983c6eef59c7bb3bb364a786e
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-01-14 07:43:07 +0000

    Create a gradient colored backgroung

---
+++ lib/gradient_container.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [color1, color2],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight)),
+    );
+  }
+}
---
+++ lib/splash.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:fun_flutter/gradient_container.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+          body: GradientContainer(Color.fromARGB(255, 9, 61, 103),
+              Color.fromARGB(255, 21, 119, 200))),
+    );
+  }
+// Remove the incomplete code block
+}
